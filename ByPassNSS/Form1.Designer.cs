8bda17a9b72fddf72e0d20b28f73bdf778f992d2
Trịnh Xuân Đạt <datbmtt1@gmail.com>
2020-12-16 20:47:50 +0700

    TXD

---
+++ ByPassNSS/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ByPassNSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConect = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtConect
+            // 
+            this.txtConect.Location = new System.Drawing.Point(82, 158);
+            this.txtConect.Name = "txtConect";
+            this.txtConect.Size = new System.Drawing.Size(93, 23);
+            this.txtConect.TabIndex = 0;
+            this.txtConect.Text = "RemovePass";
+            this.txtConect.UseVisualStyleBackColor = true;
+            this.txtConect.Click += new System.EventHandler(this.txtConect_Click);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(216, 158);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(95, 23);
+            this.btnReturn.TabIndex = 0;
+            this.btnReturn.Text = "Return_Exit";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Disconect LAN";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DisableRoom";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 235);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.txtConect);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button txtConect;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ByPassNSS/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Threading.Tasks;
+using Microsoft.Win32;
+namespace ByPassNSS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Trinh Xuan Dat
+            InitializeComponent();
+            
+        }
+        string subKey = @"SOFTWARE\NetSupport Ltd\PCICTL\ConfigList\NetSupport School";
+        string value = "";
+        string keyName = @"General\AdminPassword";
+        string keyName1 = @"General\Password";
+       
+        private void txtConect_Click(object sender, EventArgs e)
+        {
+            string data = ReadRegistry(keyName, subKey);
+            if (data != "dgAAAJ5RqmOJNaYoN)VdEUoablcA")
+            {
+                MessageBox.Show("Password already exists");
+            }
+            else
+            {
+                EditIntoRegistry(keyName, value, subKey);
+                EditIntoRegistry(keyName1, value, subKey);
+                MessageBox.Show("Success");
+            }
+         
+        }
+        static void WriteIntoRegistry(string keyName, string value, string subKey)
+        {
+
+            RegistryKey sk = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(subKey);
+            sk.SetValue(keyName, value);
+        }
+        static void EditIntoRegistry(string keyName, string value, string subKey)
+        {
+            RegistryKey sk = Registry.CurrentUser.OpenSubKey(subKey, true);
+            sk.SetValue(keyName, value);
+            sk.Close(); 
+        }
+        static string ReadRegistry(string keyName, string subKey)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(subKey, true);
+
+            string data = key.GetValue(keyName).ToString();
+            return data;
+            //key.Close();
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            string value = "dgAAAJ5RqmOJNaYoN)VdEUoablcA";
+            EditIntoRegistry(keyName,value,subKey);
+            EditIntoRegistry(keyName1, value, subKey);
+            Application.Exit();
+        }
+    }
+}
+
